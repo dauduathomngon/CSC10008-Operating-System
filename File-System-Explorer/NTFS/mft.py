2f2019387d59e06ffa244bd32dbc6384d27e5233
Le Nguyen <lenguyen18072003@gmail.com>
2023-10-26 13:56:10 +0700

    change attribute name

--- File-System-Explorer/NTFS/mft.py
+++ File-System-Explorer/NTFS/mft.py
@@ -19,44 +19,44 @@ class MFTFile:
         self.data = self.boot_sector.f.read(self.size)
 
     @property
-    def info_offset(self):
+    def info_attr_offset(self):
         # offset of first attribute ($STANDARD_INFORMATION) start at 0x14 offset and length WORD (2 byte)
         return int.from_bytes(self.data[0x14 : 0x14 + WORD], 
                               byteorder=sys.byteorder)
 
     @property
-    def info_len(self):
+    def info_attr_len(self):
         # length of ($STANDARD_INFORMATION) start at byte 4 (after offset) and length DWORD (4 byte)
         start_byte = self.info_offset + 4
         return int.from_bytes(self.data[start_byte : start_byte + (WORD * 2)], 
                               byteorder=sys.byteorder)
 
     @property
-    def name_offset(self):
+    def name_attr_offset(self):
         # offset of attribute $FILE_NAME (right after $STANDARD_INFORMATION) so it = info_len + info_offset 
         return self.info_len + self.info_offset
 
     @property
-    def name_len(self):
+    def name_attr_len(self):
         # length of ($FILE_NAME) start at byte 4 (after offset) and length DWORD (4 byte)
         start_byte = self.name_offset + 4
         return int.from_bytes(self.data[start_byte : start_byte + (WORD * 2)], 
                               byteorder=sys.byteorder)
 
     @property
-    def data_offset(self):
+    def data_attr_offset(self):
         # offset of attribute $DATA (right after $FILE_NAME)
         return self.name_offset + self.name_len
     
     @property
-    def data_len(self):
+    def data_attr_len(self):
         # length of ($DATA) start at byte 4 (after offset) and length DWORD (4 byte)
         start_byte = self.data_offset + 4
         return int.from_bytes(self.data[start_byte : start_byte + (WORD * 2)], 
                               byteorder=sys.byteorder)
 
     @property
-    def num_sector(self):
+    def num_sector_mft(self):
         # number sector of entire MFT table
         # First VCN (virtual cluster number) is 0 and Number Last VCN at byte 24 (after offset) and has length LONGLONG
         # So number of vcn = last_vcn + first_vcn + 1 (because start at 0)
