262a4f3bd3f0e434e17aa9784a26bccca958630b
Le Nguyen <lenguyen18072003@gmail.com>
2023-10-27 10:50:57 +0700

    Update main.py

--- File-System-Explorer/main.py
+++ File-System-Explorer/main.py
@@ -1,17 +1,76 @@
+import time
+from contextlib import contextmanager
 import string
 from ctypes import windll
 from typing import List
-from icecream import ic
+
+from rich.align import Align
+from rich.console import Console
+from rich.live import Live
+from rich.table import Table
+from rich.traceback import install
+from rich.prompt import Prompt
+from rich.panel import Panel
 
 from NTFS.ntfs import NTFS
+from FAT32.fat32 import FAT32
 
 # more beautiful traceback (apply globally)
-from rich.traceback import install
 install(show_locals=True, word_wrap=True)
 
-# more beautiful printing
-from rich.console import Console
-console = Console()
+GLOBAL_CONSOLE = Console()
+
+@contextmanager
+def beat(length: int = 1) -> None:
+    yield
+    time.sleep(length * 0.06)
+
+def print_member():
+    member = [
+        ["Đặng An Nguyên", "21120123"],
+        ["Phan Nguyên Phương", "21120123"],
+        ["Đỗ Hoàng Long", "21120123"],
+        ["Nguyễn Anh Tú", "21120123"],
+        ["Lê Nguyễn", "21120123"]
+    ]
+
+    table = Table(show_footer=False)
+    table_centered = Align.center(table)
+    GLOBAL_CONSOLE.clear()
+
+    with Live(table_centered, console=GLOBAL_CONSOLE, screen=False, refresh_per_second=20):
+        with beat(10):
+            table.add_column(header="Tên thành viên", no_wrap=True)
+
+        with beat(10):
+            table.add_column(header="Mã số sinh viên", no_wrap=True)
+
+        with beat(10):
+            table.title = "Danh sách các thành viên"
+
+        with beat(10):
+            table.title = (
+                "[not italic]:vampire:[/] [b green]Danh sách các thành viên [not italic]:vampire:[/]"
+            )
+
+        for row in member:
+            with beat(10):
+                table.add_row(*row)
+
+        with beat(10):
+            table.columns[0].header_style = "magenta"
+
+        with beat(10):
+            table.columns[1].header_style = "blue"
+
+        with beat(10):
+            table.caption = "From FIT HCMUS with love"
+
+        with beat(10):
+            table.caption = (
+                ":heart: From [b red not dim]FIT HCMUS with love :heart:"
+            )
+
 
 def get_drives() -> List[str]:
         """
@@ -29,10 +88,28 @@ def get_drives() -> List[str]:
         return drives
 
 if __name__=="__main__":
-    drives = get_drives()
+    # print all member
+    print_member()
 
-    if NTFS.check_ntfs(drives[2]):
-        ntfs = NTFS(drives[2])
-        # console.print(ntfs.boot_sector)
+    # print title
+    title = Align.center(Panel.fit("Đồ án HĐH: [bold]File System Explorer :smile:", 
+                                    border_style="yellow", 
+                                    padding=(0,7)))
+    GLOBAL_CONSOLE.print(title)
+
+    # print and choose volume
+    # print and choose volume
+    drives = get_drives()
+    GLOBAL_CONSOLE.print("Các volume hiện có: ")
+    for idx, drive in enumerate(drives):
+        GLOBAL_CONSOLE.print(f"{idx}) {drive}", style="cyan")
+    choice = Prompt.ask("Chọn volume mà bạn muốn :skull:", 
+                        choices=[str(i) for i in range(len(drives))],
+                        default="0") 
+
+    # check drive
+    if NTFS.check_ntfs(drives[int(choice)]):
+        vol = NTFS(drives[int(choice)])
+        GLOBAL_CONSOLE.print(vol.boot_sector)
     else:
-        exit()
+        vol = FAT32(choice)
