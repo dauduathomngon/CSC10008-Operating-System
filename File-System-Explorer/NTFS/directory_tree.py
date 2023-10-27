9474e666230398fed6af3716a93a904d29121f97
Le Nguyen <lenguyen18072003@gmail.com>
2023-10-27 10:50:52 +0700

    Update directory_tree.py

--- File-System-Explorer/NTFS/directory_tree.py
+++ File-System-Explorer/NTFS/directory_tree.py
@@ -1,16 +1,20 @@
+from collections import defaultdict
+
 from NTFS.mft_entry import MFTEntry
 from icecream import ic
+
 class DirTree:
     def __init__(self, list_entry: list[MFTEntry]) -> None:
-        self.dict_node: dict[int,MFTEntry] = {}
         self.root = None
+        self.dict_node = defaultdict(MFTEntry)
+
         #Get all node of the tree
         for entry in list_entry:
             self.dict_node[entry.id] = entry
 
         #Build the children of nodes
         for node_id in self.dict_node:
-            parent_dir_id = self.dict_node[node_id].name_attr.parent_dir_id
+            parent_dir_id = self.dict_node[node_id].parent_id
             if parent_dir_id in self.dict_node:
                 self.root = self.dict_node[parent_dir_id].childs.append(self.dict_node[node_id])
 
