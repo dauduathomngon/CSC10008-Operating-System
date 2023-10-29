52378543c0f073449ec1918738497603342cf065
Le Nguyen <lenguyen18072003@gmail.com>
2023-10-29 18:10:14 +0700

    [Done] New way to add attributes

--- File-System-Explorer/NTFS/mft_entry.py
+++ File-System-Explorer/NTFS/mft_entry.py
@@ -1,8 +1,19 @@
-import sys
+from collections import OrderedDict
 
-from const import DWORD, WORD
+from NTFS.attribute import *
 
+from icecream import ic
+
+# ------------------------------------
+# MFT Entry
+# ------------------------------------
 class MFTEntry:
+    ATTRIBUTE_MAP = {
+        0x10: StandardInfoAttrib,
+        0x30: FileNameAttrib,
+        0x80: DataAttrib
+    }
+
     def __init__(self, data) -> None:
         self.data = data
 
@@ -10,18 +21,67 @@ class MFTEntry:
         if not self.__is_valid():
             raise Exception("This is invalid entry")
 
+        # create attribute dictionary with key is attribute type
+        self.attributes = OrderedDict()
+
+        attrib_offset = self.offset_to_first_attr
+        while (True):
+            # FF FF FF FF is end of attributes
+            if int.from_bytes(self.data[attrib_offset : attrib_offset + 4], byteorder="little") == 0xFFFFFFFF:
+                break
+
+            type = Attribute.get_type(attrib_offset, data)
+
+            # We only care about $FILE_NAME, $STANDARD_INFORMATION and $DATA
+            if type not in MFTEntry.ATTRIBUTE_MAP:
+                pass
+
+            # We only care about first $DATA attribute, because it contains real data
+            if type == 0x80:
+                self.attributes[type] = MFTEntry.ATTRIBUTE_MAP[type](attrib_offset, data)
+                break
+
+            # Here, we take $STANDARD_INFORMATION and $FILE_NAME attribute
+            self.attributes[type] = MFTEntry.ATTRIBUTE_MAP[type](attrib_offset, data)
+
+            attrib_offset += self.attributes[type].length
+
+    # ------------------------------------
+    # Property
+    # ------------------------------------
+    @property
+    def signature(self) -> str:
+        # signature of an entry at offset 0 and has length 4
+        return self.data[0 : 0 + 4].decode("utf-8")
+
+    @property
+    def offset_to_first_attr(self) -> int:
+        # first attribute offset at offset 0x14 and has length 2
+        return int.from_bytes(self.data[0x14 : 0x14 + 2],
+                              byteorder="little")
+
+    @property
+    def id(self) -> int:
+        # ID of entry at offset 0x2C and has length 4
+        return int.from_bytes(self.data[0x2C : 0x2C + 4],
+                              byteorder="little")
+
+    @property
+    def flag(self) -> int:
+        # flag of entry at offset 0x16 and has length 4
+        return int.from_bytes(self.data[0x16 : 0x16 + 4],
+                              byteorder="little")
+
+    # ------------------------------------
+    # Method
+    # ------------------------------------
     def __is_valid(self) -> bool:
         if self.signature == "FILE":
             return True
         return False
 
-    @property
-    def signature(self) -> str:
-        # signature of an entry at offset 0 and has length DWORD (4 bytes)
-        return self.data[0 : 0 + DWORD].decode("utf-8")
+    def is_directory(self) -> bool:
+        return (self.flag == 3)
 
-    @property
-    def first_attribute_offset(self) -> int:
-        # first attribute offset at offset 0x14 and has length WORD
-        return int.from_bytes(self.data[0x14 : 0x14 + WORD],
-                              byteorder="little")
+    def is_file(self) -> bool:
+        return (self.flag == 1)
