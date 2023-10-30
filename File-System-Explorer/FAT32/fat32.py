7b9d91057aa8fd722c3130abfd12824dbef17315
Le Nguyen <lenguyen18072003@gmail.com>
2023-10-30 09:15:55 +0700

    Merge "main" fat32.py

--- File-System-Explorer/FAT32/fat32.py
+++ File-System-Explorer/FAT32/fat32.py
@@ -1,119 +1,153 @@
-import sys
-
-class FAT:
-    def __init__(self, data) -> None:
-        self.entry_size = 4
-        self.elements=[]
-        self.data = data
-        for i in range(0, len(data),self.entry_size):
-            self.elements.append(int.from_bytes(self.data[i:i+4],byteorder=sys.byteorder))
-
-    def get_chain(self, index:int)-> list[int]:
-        index_list = []
-        while True:
-            index_list.append(index)
-            index = self.elements[index]
-            if index == 0x0FFFFFFF or index == 0x0FFFFFF7:
-                break
-            return index_list 
-
+from FAT32.bootsector import BootSector
+from FAT32.fat import FAT
+from FAT32.rdet import RDET
+import re
 class FAT32:
-    # Constructor - Format name example: "D:"
-    def __init__(self, name:str) -> None:
-        self.name = name
+    def __init__(self,vol_name) -> None:
+        self.name= vol_name
         try:
             self.fd= open(r'\\.\%s'%self.name,'rb')
-        # Handle error
         except FileNotFoundError:
-            print(f"[ERROR] No volume named {name}")
+            print(f"[ERROR] No volume named {self.name}")
             exit()
         except PermissionError:
-            print("[ERROR] Permission denied, try again as administrator/root")
+            print("[ERROR] Permission denied, try again as admin/root")
             exit()
         except Exception as e:
             print(e)
-            print(f"[ERROR] {e} ")
+            print("[ERROR] Unknown error occurred")
             exit() 
-            
         try:
-            # Read 512 bytes (0x200 hex = 512 dec)
-            self.boot_sector_raw = self.fd.read(0x200)
-            # Declare dictionary to save data
-            self.boot_sector = {}
-            # Reading bootsector (Read and convert boot_sector_raw)
-            self.reading_boot_sector()
-            # Read FAT table
-            self.FAT_size
-            self.FAT_table:list[FAT]=[]
-            self.readFAT_table()
-            
+            boot_sector_raw = self.fd.read(0x200)
         except Exception as e:
             print(f"[Error] {e}")
+            exit()
             
+        # Read bootsector
+        self.bootsector = BootSector(boot_sector_raw)
+        
+        # Read reversed sector
+        self.reversed_sector = self.fd.read(self.bootsector.bytes_per_sector*(self.bootsector.reserved_sectors-1))
+        
+        # Read FAT table
+        self.FAT_table:list[FAT]=[]
+        self.read_FAT_table()
+        
+        # Read RDET
+        self.DET={} # To archive DET of all folder in Root directory
+        self.RDET= RDET(self.get_cluster_data(self.bootsector.start_cluster_RDET))
+        self.DET[self.bootsector.start_cluster_RDET] =self.RDET
+    
+    
+    
+    """ Methods """
+    # Parse path
+    def parse_path(self, path):
+        dirs = re.sub(r"[/\\]+", r"\\", path).strip("\\").split("\\")
+        return dirs
+    
+    # Cluster to sector index
+    def cluster_to_sector(self,clusterIndex):
+        return self.bootsector.reserved_sectors+self.bootsector.number_of_FAT_table*self.bootsector.sectors_per_FAT+(clusterIndex-2)*self.bootsector.sectors_per_cluster
+    
+    
+    # Read FAT table
+    def read_FAT_table(self):
+        FAT_size = self.bootsector.sectors_per_FAT* self.bootsector.bytes_per_sector
+        for _ in range(self.bootsector.number_of_FAT_table):
+            self.FAT_table.append(FAT(self.fd.read(FAT_size)))
+
+    # Get cluster data
+    def get_cluster_data(self, clusterIndex):
+        index_list=self.FAT_table[0].get_chain(clusterIndex)
+        data= b""
+        for i in index_list:
+            offset = self.cluster_to_sector(i)
+            self.fd.seek(offset*self.bootsector.bytes_per_sector)
+            data+=self.fd.read(self.bootsector.sectors_per_cluster*self.bootsector.bytes_per_sector)
+        return data
+          
+    # Visit dir
+    def visit_dir(self,path)-> RDET:
+        if path=="":
+            raise Exception("Directory name is required!")
+        dirs = self.parse_path(path)
+        
+        if dirs[0]==self.name:
+            currentDET = self.RDET
+            dirs.pop(0)
+        else:
+            currentDET= self.RDET
+        
+        for d in dirs:
+            entry = currentDET.find_entry(d)
+            if entry is None:
+                raise Exception("Directory not found!")
+            if entry.is_directory():
+                # If entry is RDET
+                if entry.start_cluster==0:
+                    currentDET= self.DET[self.bootsector.start_cluster_RDET]
+                    continue
+                # If exists entry before => Saving time for reading again DET
+                if entry.start_cluster in self.DET:
+                    currentDET = self.DET[entry.start_cluster]
+                    continue
+                # If not exists, read DET of folder
+                self.DET[entry.start_cluster]=RDET(self.get_cluster_data(entry.start_cluster))
+                # Set folder to current DET 
+                currentDET = self.DET[entry.start_cluster]
+            else:
+                raise Exception("Not a directory")
+        return currentDET
+                
+    # Get directory
+    def get_dir(self, path):
+        try:
+            if path!="":
+                currentDET= self.visit_dir(path)
+                entry_list = currentDET.get_active_entries()
+                
+            else:
+                entry_list= self.RDET.get_active_entries()
+            ret = []
+            for entry in entry_list:
+                obj={}
+                obj["Flags"]= entry.attr.value
+                obj["Date Modified"] = entry.date_updated
+                obj["Size"] = entry.size
+                obj["Name"] = entry.long_name
+                if entry.start_cluster == 0:
+                    obj["Sector"] = (entry.start_cluster + 2) * self.bootsector.sectors_per_cluster
+                else:
+                    obj["Sector"] = entry.start_cluster * self.bootsector.sectors_per_cluster
+                ret.append(obj)
+            return ret
+        except Exception as e:
+            raise(e)
+        
+    # Change dir
+    def change_dir(self, path=""):
+        if path == "":
+            raise Exception("Path to directory is required!")
+        currentDET = self.visit_dir(path)
+        self.RDET = currentDET    
             
-    # Check if FAT32
+    # Check FAT32         
     @staticmethod
     def check_fat32(name: str):
         try:
             with open(r'\\.\%s' % name, 'rb') as fd:
                 fd.read(1)
-                # Move cursor to offset 0x52
                 fd.seek(0x52)
-                # Read 8 bytes
                 fat_name = fd.read(8)
                 if fat_name == b"FAT32   ":
                     return True
                 return False
         except Exception as e:
             print(f"[ERROR] {e}")
-            exit()
-           
-    # Reading boot sector
-    def reading_boot_sector(self):
-         # Jump Boot code: Offset 0 size 3
-        self.boot_sector['Jump_Code'] = self.boot_sector_raw[:3]
-        # OEM Name: offset 3 size 8
-        self.boot_sector['OEM_ID'] = self.boot_sector_raw[3:0xB]
-        # Bytes per sector: offset B size 2
-        self.boot_sector['Bytes Per Sector'] = int.from_bytes(self.boot_sector_raw[0xB:0xD], byteorder=sys.byteorder)
-        # Sector per cluster: offset D size 1
-        self.boot_sector['Sectors Per Cluster'] = int.from_bytes(self.boot_sector_raw[0xD:0xE], byteorder=sys.byteorder)
-        # Reserved sectors count: offset E size 2
-        self.boot_sector['Reserved Sectors'] = int.from_bytes(self.boot_sector_raw[0xE:0x10], byteorder=sys.byteorder)
-        # Number of FAT tables: offset 10 size 1
-        self.boot_sector['No. Copies of FAT'] = int.from_bytes(self.boot_sector_raw[0x10:0x11], byteorder=sys.byteorder)
-        # Volume type: offset 15 size 1
-        self.boot_sector['Media Descriptor'] = self.boot_sector_raw[0x15:0x16]
-        # Sectors per track: offset 18 size 2
-        self.boot_sector['Sectors Per Track'] = int.from_bytes(self.boot_sector_raw[0x18:0x1A], byteorder=sys.byteorder)
-        # Number of heads: offset 1A size 2
-        self.boot_sector['No. Heads'] = int.from_bytes(self.boot_sector_raw[0x1A:0x1C], byteorder=sys.byteorder)
-        # Number of sectors in volume: offset 20 size 4
-        self.boot_sector['No. Sectors In Volume'] = int.from_bytes(self.boot_sector_raw[0x20:0x24], byteorder=sys.byteorder)
-        # Number of sectors in a FAT table: offset 24 size 4
-        self.boot_sector['Sectors Per FAT'] = int.from_bytes(self.boot_sector_raw[0x24:0x28], byteorder=sys.byteorder)
-        # Ext Flags: offset 28 size 2
-        self.boot_sector['Flags'] = int.from_bytes(self.boot_sector_raw[0x28:0x2A], byteorder=sys.byteorder)
-        # FAT32 version: offset 2A size 2
-        self.boot_sector['FAT32 Version'] = self.boot_sector_raw[0x2A:0x2C]
-        # Starting cluster RDET: offset 2C size 2
-        self.boot_sector['Starting Cluster of RDET'] = int.from_bytes(self.boot_sector_raw[0x2C:0x30], byteorder=sys.byteorder)
-        # Sector Number of the FileSystem Information Sector: offset 30 size 2
-        #self.boot_sector['Sector Number of the FileSystem Information Sector'] = self.boot_sector_raw[0x30:0x32]
-        # Sector Number of BackupBoot: offset 32 size 2
-        #self.boot_sector['Sector Number of BackupBoot'] = self.boot_sector_raw[0x32:0x34]
-        # FAT type: offset 52 size 8
-        self.boot_sector['FAT Name'] = self.boot_sector_raw[0x52:0x5A]
-        # Boot code: offset 5A siuze 420
-        #self.boot_sector['Executable Code'] = self.boot_sector_raw[0x5A:0x1FE]
-        # Signature: offset 1FE size 2
-        self.boot_sector['Signature'] = self.boot_sector_raw[0x1FE:0x200]
-        # Starting sector of data
-        self.boot_sector['Starting Sector of Data'] = self.boot_sector['Reserved Sectors'] + self.boot_sector['No. Copies of FAT'] * self.boot_sector['Sectors Per FAT']
-        
-        
-    # Read FAT table
-    def readFAT_table(self)->None:
-            self.FAT_size = self.boot_sector['Bytes Per Sector']*self.boot_sector['Sectors Per FAT']
-            for _ in range(self.boot_sector['No. Copies of FAT']):
-                self.FAT_table.append(FAT(self.fd.read(self.FAT_size)))
+            exit()    
+    
+    def __del__(self):
+        if getattr(self, "fd", None):
+            print("Closing Volume...")
+            self.fd.close()
