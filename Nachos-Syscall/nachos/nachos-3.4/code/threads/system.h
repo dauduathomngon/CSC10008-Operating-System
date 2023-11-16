d79ddadef1f10fd0dd74a5e42434a179fedb3a7e
dauduathomngon <lenguyen18072003@gmail.com>
2023-11-16 14:25:58 +0700

    Make synchcons.h and synchcons.cc work with system

--- Nachos-Syscall/nachos/nachos-3.4/code/threads/system.h
+++ Nachos-Syscall/nachos/nachos-3.4/code/threads/system.h
@@ -31,6 +31,9 @@ extern Timer *timer;				// the hardware alarm clock
 
 #ifdef USER_PROGRAM
 #include "machine.h"
+#include "synchcons.h"
+#include "synch.h"
+extern SynchConsole* synchcons;
 extern Machine* machine;	// user program memory and registers
 #endif
 
