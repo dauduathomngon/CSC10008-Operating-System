ed1a9630cba839886e91b8bc05cb182857ef312a
Le Nguyen <lenguyen18072003@gmail.com>
2023-11-16 15:09:12 +0700

    Update run.sh

--- Nachos-Syscall/nachos/nachos-3.4/code/run.sh
+++ Nachos-Syscall/nachos/nachos-3.4/code/run.sh
@@ -5,7 +5,7 @@ make
 if [ $? -eq 0 ]; then
     echo $'\n-------------------------------------- DONE --------------------------------------'
     echo $'Here is output: \n'
-    ./userprog/nachos -rs 1023 -x ./test/testFunction
+    ./userprog/nachos -rs 1023 -x ./test/$1
 else
     echo $'\n-------------------------------------- FAILED -------------------------------------- \n'
 fi
