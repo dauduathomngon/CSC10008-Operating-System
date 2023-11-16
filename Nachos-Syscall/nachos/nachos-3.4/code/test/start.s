c4af96d4f6646c69883c1166967bd9373dd61564
dauduathomngon <lenguyen18072003@gmail.com>
2023-11-16 14:30:01 +0700

    Add PrintChar PrintInt PrintString ReadChar

--- Nachos-Syscall/nachos/nachos-3.4/code/test/start.s
+++ Nachos-Syscall/nachos/nachos-3.4/code/test/start.s
@@ -130,6 +130,30 @@ Yield:
 	j	$31
 	.end Yield
 
+	.globl PrintString
+	.ent	PrintString
+PrintString:
+	addiu $2,$0,SC_PrintString
+	syscall
+	j	$31
+	.end PrintString
+
+	.globl PrintChar
+	.ent	PrintChar
+PrintChar:
+	addiu $2,$0,SC_PrintChar
+	syscall
+	j	$31
+	.end PrintChar
+
+	.globl PrintInt
+	.ent	PrintInt
+PrintInt:
+	addiu $2,$0,SC_PrintInt
+	syscall
+	j	$31
+	.end PrintInt
+
 /* dummy function to keep gcc happy */
         .globl  __main
         .ent    __main
