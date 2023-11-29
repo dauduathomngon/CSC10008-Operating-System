aede9a38d85afc7da912066bcb1ea15171155203
Le Nguyen <lenguyen18072003@gmail.com>
2023-11-29 11:20:39 +0700

    Update help.c

--- Nachos-Syscall/nachos/nachos-3.4/code/test/help.c
+++ Nachos-Syscall/nachos/nachos-3.4/code/test/help.c
@@ -25,6 +25,7 @@ int main()
     PrintString("Cac chuong trinh hien co: \n");
     PrintString("  + ascii: In ra bang ma ASCII \n");
     PrintString("  + sort: Sap xep lai mang n so nguyen (n <= 100)\n");
+    PrintString("  + testFunction: test cac chuc nang ReadInt, PrintInt, ...\n");
     PrintString("Lam sao de chay chuong trinh, ta co 2 cach: \n");
     PrintString("  + Cach 1: Vao thu muc code, chay 'bash run.sh <ten chuong trinh>' \n");
     PrintString("  + Cach 2: Vao thu muc code, chay 'make' va sau do chay './userprog/nachos -rs 1023 -x ./test/<ten chuong trinh>'\n");
