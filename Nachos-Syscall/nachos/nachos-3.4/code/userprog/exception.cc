5e016e49fa96e27c2b55e01d5e2242d04a8a82f5
Le Nguyen <lenguyen18072003@gmail.com>
2023-11-29 11:09:17 +0700

    Update exception.cc

--- Nachos-Syscall/nachos/nachos-3.4/code/userprog/exception.cc
+++ Nachos-Syscall/nachos/nachos-3.4/code/userprog/exception.cc
@@ -124,11 +124,16 @@ int System2User(int virtAddr, int len, char *buffer)
 // Tang thanh ghi
 void IncreasePC()
 {
-    // Plus 4 for moving to next command
-    int nextPC = machine->registers[NextPCReg] + 4;
-    machine->registers[PrevPCReg] = machine->registers[PCReg];
-    machine->registers[PCReg] = machine->registers[NextPCReg];
-    machine->registers[NextPCReg] = nextPC;
+    //     // Plus 4 for moving to next command
+    //     int nextPC= machine->registers[NextPCReg]+4;
+    //     machine->registers[PrevPCReg]=machine->registers[PCReg];
+    //     machine->registers[PCReg]=machine->registers[NextPCReg];
+    //     machine->registers[NextPCReg]= nextPC;
+    int counter = machine->ReadRegister(PCReg);
+    machine->WriteRegister(PrevPCReg, counter);
+    counter = machine->ReadRegister(NextPCReg);
+    machine->WriteRegister(PCReg, counter);
+    machine->WriteRegister(NextPCReg, counter + 4);
 }
 
 void ExceptionHandler(ExceptionType which)
@@ -143,7 +148,7 @@ void ExceptionHandler(ExceptionType which)
     case PageFaultException:
     {
         DEBUG('a', "\nPage fault.");
-        printf("\n\nPage fault.");
+        printf("\nPage fault.\n");
         interrupt->Halt();
         break;
     }
@@ -151,7 +156,7 @@ void ExceptionHandler(ExceptionType which)
     case ReadOnlyException:
     {
         DEBUG('a', "\nPage marked read-only");
-        printf("\n\nPage marked read-only");
+        printf("\nPage marked read-only\n");
         interrupt->Halt();
         break;
     }
@@ -159,7 +164,7 @@ void ExceptionHandler(ExceptionType which)
     case BusErrorException:
     {
         DEBUG('a', "\nInvalid physical address");
-        printf("\n\nInvalid physical address");
+        printf("\nInvalid physical address.\n");
         interrupt->Halt();
         break;
     }
@@ -167,7 +172,7 @@ void ExceptionHandler(ExceptionType which)
     case AddressErrorException:
     {
         DEBUG('a', "\n Address error.");
-        printf("\n\n Address error.");
+        printf("\n Address error.\n");
         interrupt->Halt();
         break;
     }
@@ -175,15 +180,15 @@ void ExceptionHandler(ExceptionType which)
     case OverflowException:
     {
         DEBUG('a', "\nOverflow !!!");
-        printf("\n\nOverflow !!!");
+        printf("\nOverflow !!!\n");
         interrupt->Halt();
         break;
     }
 
     case IllegalInstrException:
     {
-        DEBUG('a', "\nIllegal instr.");
-        printf("\n\nIllegal instr.");
+        DEBUG('a', "\nIllegal instruction.");
+        printf("\nIllegal instruction.\n");
         interrupt->Halt();
         break;
     }
@@ -191,7 +196,7 @@ void ExceptionHandler(ExceptionType which)
     case NumExceptionTypes:
     {
         DEBUG('a', "\nNumber exception types");
-        printf("\n\nNumber exception types");
+        printf("\nNumber exception types\n");
         interrupt->Halt();
         break;
     }
@@ -202,6 +207,10 @@ void ExceptionHandler(ExceptionType which)
         {
         case SC_Halt:
         {
+            /* Summary: Ham dung de tat chuong trinh
+             * Input: Khong co
+             * Output: Thong bao tat chuong trinh
+             */
             DEBUG('a', "Shutdown, initiated by user program.\n");
             interrupt->Halt();
             break;
@@ -215,15 +224,22 @@ void ExceptionHandler(ExceptionType which)
 
         case SC_PrintString:
         {
+            /* Summary: Ham dung de in ra string
+             * Input: char* buffer (string duoc nhap vao)
+             * Output: In ra string duoc nhap vao
+             */
             int virtualAddress;
             char *buf;
 
+            // Lấy địa chỉ từ thanh ghi thứ 4
             virtualAddress = machine->ReadRegister(4);
 
+            // Biến buf của kernel lấy từ user (địa chỉ chuỗi từ thanh
+            // ghi thứ 4)
             buf = User2System(virtualAddress, 255);
 
             int size = 0;
-            while (buf[size] != '\0' && size <255)
+            while (buf[size] != '\0')
                 size++;
 
             synchcons->Write(buf, size + 1);
@@ -236,6 +252,10 @@ void ExceptionHandler(ExceptionType which)
 
         case SC_ReadChar:
         {
+            /* Summary: Ham dung de doc mot ky tu (char)
+             * Input: Khong co
+             * Output: Tra ve ky tu doc duoc (neu khong phai ky tu hop le, tra ve ky tu rong)
+             */
             char *buffer = new char[255 + 1]();
             int length = synchcons->Read(buffer, 255);
 
@@ -262,6 +282,10 @@ void ExceptionHandler(ExceptionType which)
 
         case SC_PrintChar:
         {
+            /* Summary: Ham dung de in ra ky tu (char)
+             * Input: char* c (ky tu duoc nhap vao)
+             * Output: In ra ky tu duoc nhap vao
+             */
             char c = (char)machine->ReadRegister(4);
 
             synchcons->Write(&c, 1);
@@ -270,9 +294,12 @@ void ExceptionHandler(ExceptionType which)
             break;
         }
 
-        // => Cac anh se viet tiep o day
         case SC_PrintInt:
         {
+            /* Summary: Ham dung de in ra so nguyen (int)
+             * Input: int input (so nguyen duoc nhap vao)
+             * Output: In ra so nguyen duoc nhap vao
+             */
             int number = machine->ReadRegister(4);
             if (number < 10 && number >= 0)
             {
@@ -320,11 +347,19 @@ void ExceptionHandler(ExceptionType which)
 
         case SC_ReadString:
         {
+            /* Summary: Ham dung de doc mot string
+             * Input: Khong co
+             * Output: Tra ve string doc duoc
+             */
             int virtualAddress = machine->ReadRegister(4);
             int length = machine->ReadRegister(5);
-            char *buffer = User2System(virtualAddress, length);
+
+            char *buffer = new char[length + 1]();
+            buffer = User2System(virtualAddress, length);
+
             synchcons->Read(buffer, length);
             System2User(virtualAddress, length, buffer);
+
             delete buffer;
             IncreasePC();
             break;
@@ -332,6 +367,11 @@ void ExceptionHandler(ExceptionType which)
 
         case SC_ReadInt:
         {
+            /* Summary: Ham dung de doc mot so nguyen (int)
+             * Input: Khong co
+             * Output: Tra ve so nguyen doc duoc (neu khong phai so nguyen hop le, tra ve 0)
+             * Note: Ngoai ra so -354.0000000 cung duoc tinh la so nguyen -354
+             */
             char *buffer = new char[255 + 1]();
             int maxBuffer = 255;
             int sign = 1;
@@ -348,6 +388,7 @@ void ExceptionHandler(ExceptionType which)
                 start = 1;
             }
 
+            // check invalid number
             for (i = start; i < size; i++)
             {
                 if (buffer[i] < '0' || buffer[i] > '9')
@@ -378,7 +419,36 @@ void ExceptionHandler(ExceptionType which)
                 }
             }
 
-            for (i = start; i < size - after_dot; i++)
+            // prevent overflow
+            char *max_int = "2147483647";
+            int max_length = 10;
+            int real_size = size - after_dot;
+
+            if (((start == 1) && (real_size == max_length + 1)) || ((start == 0) && (real_size == max_length)))
+            {
+                for (i = 0; i < max_length; i++)
+                {
+                    if (buffer[i + start] - '0' > max_int[i] - '0')
+                    {
+                        DEBUG('a', "\nERROR: Overflow!");
+                        machine->WriteRegister(2, 0);
+                        delete buffer;
+                        IncreasePC();
+                        return;
+                    }
+                }
+            }
+            else if (((start == 1) && (real_size > max_length + 1)) || ((start == 0) && (real_size > max_length)))
+            {
+                DEBUG('a', "\nERROR: Overflow!");
+                machine->WriteRegister(2, 0);
+                delete buffer;
+                IncreasePC();
+                return;
+            }
+
+            // turn buffer to number
+            for (i = start; i < real_size; i++)
             {
                 result = result * 10 + (buffer[i] - '0');
             }
